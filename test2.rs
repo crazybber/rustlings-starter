77612ca1f495df4d8f3e62e3628782a64e1292cc
Edward <chenlulei@gmail.com>
2020-03-29 23:16:08 +0800

    update testing2

--- test2.rs
+++ test2.rs
@@ -17,14 +17,33 @@ fn string(arg: String) {
 }
 
 fn main() {
-    ("blue");
-    ("red".to_string());
-    (String::from("hi"));
-    ("rust is fun!".to_owned());
-    ("nice weather".into());
-    (format!("Interpolation {}", "Station"));
-    (&String::from("abc")[0..1]);
-    ("  hello there ".trim());
-    ("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ("mY sHiFt KeY iS sTiCkY".to_lowercase());
+
+    (string_slice("blue"));
+
+    (string("red".to_string()));
+    //string
+    (string(String::from("hi")));
+
+    (string("rust is fun!".to_owned()));
+
+    //string
+    (string("nice weather".into()));
+
+    (string_slice("nice weather".into()));
+
+    //this is a String
+    (string(format!("Interpolation {}", "Station")));
+
+    //Slice
+    (string_slice(&String::from("abc")[0..1]));
+
+    //Slice
+    (string_slice("  hello there ".trim()));
+
+
+    (string("Happy Monday!".to_string().replace("Mon", "Tues")));
+
+    (string("mY sHiFt KeY iS sTiCkY".to_lowercase()));
+
 }
+
