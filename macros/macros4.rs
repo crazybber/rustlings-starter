16861b061d88758fa65651a49e4be13ba974ccad
Edward <crazybber@outlook.com>
2020-03-30 12:38:12 +0800

    finish macro and test4

--- macros/macros4.rs
+++ macros/macros4.rs
@@ -1,15 +1,29 @@
 // macros4.rs
 // Make me compile! Execute `rustlings hint macros4` for hints :)
 
-// I AM NOT DONE
 
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    }; //here need a ";"
+
+
+    // this is a capture expression ,following are allowed.
+    /*
+    item: an item, like a function, struct, module, etc.
+    block: a block (i.e. a block of statements and/or an expression, surrounded by braces)
+    stmt: a statement
+    pat: a pattern
+    expr: an expression
+    ty: a type
+    ident: an identifier
+    path: a path (e.g. , , , …)foo::std::mem::replacetransmute::<_, int>
+    meta: a meta item; the things that go inside and attributes#[...]#![...]
+    tt: a single token tree
+    */
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
-    }
+    };
 }
 
 fn main() {
--- test4.rs
+++ test4.rs
@@ -3,9 +3,14 @@
 // - Modules
 // - Macros
 
+
 // Write a macro that passes the test! No hints this time, you can do it!
 
-// I AM NOT DONE
+macro_rules! my_macro {
+    ($string:expr) => {
+        format!("Hello {}",$string)
+    }
+}
 
 #[cfg(test)]
 mod tests {
